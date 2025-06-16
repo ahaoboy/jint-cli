22c7ab6b4bb006eea78898fbf9131e78f7ebff1c
ahaoboy <504595380@qq.com>
2025-06-16 12:36:35 +0800

    init

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using Jint;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Usage: jint <script.js>");
+            return;
+        }
+
+        string scriptPath = args[0];
+
+        if (!File.Exists(scriptPath))
+        {
+            Console.WriteLine($"Error: File '{scriptPath}' not found.");
+            return;
+        }
+
+        try
+        {
+            string scriptContent = File.ReadAllText(scriptPath);
+
+            var engine = new Engine()
+                .SetValue("console", new
+                {
+                    log = new Action<object>(obj => Console.WriteLine(obj)),
+                    error = new Action<object>(obj => Console.Error.WriteLine(obj))
+                });
+
+            engine.Execute(scriptContent);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error executing script: {ex.Message}");
+        }
+    }
+}
